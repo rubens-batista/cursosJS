04455086e65ea722f270ddecff5bebeb902a5527
Rubens Batista <nunobfc24@gmail.com>
2020-05-04 21:23:02 +0100

    add aula17

---
+++ Anotaçoes/objetos.js
@@ -0,0 +1,11 @@
+//Objetos
+/*
+    //São váriaveis mais poderosas, capaz de guarda até functions.
+
+    exemplo{
+        let amigo = {nome:'José', sexo:'M', peso:85.4, engordar(p){}}
+
+        //Foram criados 4 slots dentro do objeto 'amigo'
+    }
+    
+*/
